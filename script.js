89ecfc0b2a53fb0e0ce5b3593b4045e079fd7da6
yohanês <yzanghelini@gmail.com>
2025-03-13 18:42:00 -0300

    feature: ponteiros rotacionam de acordo com a hora

--- script.js
+++ script.js
@@ -5,11 +5,35 @@ const minutoDigital = document.querySelector('.minuto');
 const horaAnal = document.querySelector('.horaAnal');
 const minutoAnal = document.querySelector('.minutoAnal');
 const segundoAnal = document.querySelector('.segundoAnal');
+const turno = document.querySelector('.turno');
+
+function verificarTurno(hora) {
+    hora < 12 ? turno.innerText = 'AM' : turno.innerText = 'PM'; 
+}
 
 function atualizarRelogio() {
+
    const data = new Date();
-   horaDigital.innerText = String(data.getHours()).padStart(2, "0");
-   minutoDigital.innerText = String(data.getMinutes()).padStart(2, "0");
+   let hora = String(data.getHours());
+   let minuto = String(data.getMinutes());
+   let segundo = String(data.getSeconds());
+
+   verificarTurno(hora);
+   horaDigital.innerText = hora.padStart(2, "0");
+   minutoDigital.innerText = minuto.padStart(2, "0");
+
+   let grausSegundo = 6 * segundo - 90;
+   let grauMinuto = (6 * minuto) + (segundo * 0.1)  - 90;
+   let grauHoras = (30 * hora) + (minuto * 0.5)  - 90; 
+
+   console.log(`grauSegundos: ${grausSegundo}`);
+   console.log(`grauMinutos: ${grauMinuto}`);
+   console.log(`grauHoras: ${grauHoras}`);
+
+   horaAnal.style.transform = `rotate(${grauHoras}deg)`;
+   minutoAnal.style.transform = `rotate(${grauMinuto}deg)`;
+   segundoAnal.style.transform = `rotate(${grausSegundo}deg)`;
+
 }
 
 // const botaoAnt = document.querySelector('.antPainel');
